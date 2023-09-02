ab7b82544497b70f5e67a8e7422f4831703a3ac3
Christian Nwachukwu <chrisnwachukwu107@gmail.com>
2023-09-02 17:55:15 +0100

    Final Project

--- dcrm/settings.py
+++ dcrm/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = open("C:\\Users\\user\\Desktop\\backend-projects\\django_crm\\dj_secret_key", "r").read()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', '.now.sh']
 
 
 # Application definition
